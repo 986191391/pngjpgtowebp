38450b7386c7bba9615495f3811edd64158d5a0e
yangzefeng <986191391@qq.com>
2023-07-18 11:32:58 +0800

    jpeg

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import imagemin from 'imagemin';
 import imageminWebp from 'imagemin-webp';
 
 (async () => {
-	await imagemin(['images/*.{jpg,png}'], {
+	await imagemin(['images/*.{jpg,jpeg,png}'], {
 		destination: 'images/build',
 		plugins: [
 			imageminWebp({ quality: 40 })
