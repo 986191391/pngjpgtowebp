c7a7f345a3cd915b4c4c2828788dadac4f24c876
yangzefeng <986191391@qq.com>
2023-08-08 21:51:44 +0800

    脚本加入用户输入图片转换质量

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 import imagemin from 'imagemin';
 import imageminWebp from 'imagemin-webp';
+import ReadLine from 'readline';
 
-(async () => {
+const readline = ReadLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question(`请输入转换质量(1-100)：`, (quality = 50) => {
+  if (!quality) quality = 50;
+  if (quality < 1 || quality > 100) {
+    console.log('请输入正确的转换质量！');
+    return readline.close();
+  }
+  console.log(`--------转换中...--------`);
+  transImage(quality);
+  readline.close();
+});
+
+async function transImage (quality) {
 	await imagemin(['images/*.{jpg,jpeg,png}'], {
 		destination: 'images/build',
 		plugins: [
-			imageminWebp({ quality: 40 })
+			imageminWebp({ quality: quality })
 		]
 	});
 
-	console.log('Images optimized');
-})();
+  console.log(`--------转换完成！--------`);
+}
