18645a6695d4c0b61f60e389b3981170d25a3ea3
yangzefeng <986191391@qq.com>
2023-08-16 15:13:20 +0800

    加入png/jpg/jpeg的图片压缩，脚本加入压缩和webp格式转换两种模式

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 import imagemin from 'imagemin';
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminPngquant from 'imagemin-pngquant'; 
 import imageminWebp from 'imagemin-webp';
 import ReadLine from 'readline';
 
@@ -7,18 +9,47 @@ const readline = ReadLine.createInterface({
   output: process.stdout,
 });
 
-readline.question(`请输入转换质量(1-100)：`, (quality = 50) => {
-  if (!quality) quality = 50;
-  if (quality < 1 || quality > 100) {
-    console.log('请输入正确的转换质量！');
-    return readline.close();
+readline.question('请选择执行模式 -压缩图片(y), -格式转换Webp(t)。', (model = 'y') => {
+  if (model === 't') {
+    readline.question('请输入图片转换质量(1-100)：', (quality = 50) => {
+      if (!quality) quality = 50;
+      if (quality < 1 || quality > 100) {
+        console.log('请输入正确的转换质量！');
+        return readline.close();
+      }
+      console.log(`--------转换中...--------`);
+      transToWebp();
+      readline.close();
+    })
+  } else {
+    readline.question('请输入压缩质量(1-100)：', (quality = 50) => {
+      if (!quality) quality = 50;
+      if (quality < 1 || quality > 100) {
+        console.log('请输入正确的压缩质量！');
+        return readline.close();
+      }
+      console.log(`--------压缩中...--------`);
+      compress(quality);
+      readline.close();
+    })
   }
-  console.log(`--------转换中...--------`);
-  transImage(quality);
-  readline.close();
 });
 
-async function transImage (quality) {
+// 压缩图片
+async function compress (quality) {
+	await imagemin(['images/*.{jpg,jpeg,png}'], {
+		destination: 'images/build',
+		plugins: [
+      imageminPngquant({ quality: [0.4, 0.8] }),
+      imageminMozjpeg({ quality: quality })
+		]
+	});
+
+  console.log(`--------压缩完成！--------`);
+}
+
+// 转换为webp格式
+async function transToWebp (quality) {
 	await imagemin(['images/*.{jpg,jpeg,png}'], {
 		destination: 'images/build',
 		plugins: [
